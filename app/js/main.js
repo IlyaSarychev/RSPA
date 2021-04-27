8f1c9873cd4d53201bf55b5aa279aefb2a92341d
Ilya <sarychev_ilya@list.ru>
2021-04-27 14:57:56 +0500

    section work programm

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
             $('.header').removeClass('header--scroll')
         }
     })
+
+    let workprogSlider = $('.workprog-slider').slick({
+        autoplay: false,
+        arrows: false
+    })
+
+    $('.workprog-slider-arrows .arrow--prev').click(() => {
+        workprogSlider.slick('slickPrev')
+    })
+
+    $('.workprog-slider-arrows .arrow--next').click(() => {
+        workprogSlider.slick('slickNext')
+    })
 })
