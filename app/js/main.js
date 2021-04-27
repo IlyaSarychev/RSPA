f093d0e843d7593338fff28b86283b5c12d3c9f5
Ilya <sarychev_ilya@list.ru>
2021-04-27 13:03:12 +0500

    nomination section

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 100) {
+            $('.header').addClass('header--scroll')
+        } else {
+            $('.header').removeClass('header--scroll')
+        }
+    })
+})
